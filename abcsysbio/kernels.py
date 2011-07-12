c04c64ea72be1f732fedfe0236605ba28a37b011
finalstate <finalstate@a9791ac1-d34e-45f5-b297-f83e2f7b00e5>
2011-07-12 17:24:17 +0000

    Added kernel auxilliary information, normal priors, weighted variance for normal kernels

--- abcsysbio/kernels.py
+++ abcsysbio/kernels.py
@@ -1,85 +1,129 @@
 # module defining all things kernel
 
+# kernel_type = 1 : component-wise uniform kernels
+# kernel_type = 2 : component-wise normal kernels
+
+import numpy
 from numpy import random as rnd
 from scipy.stats import norm
 from abcsysbio import statistics
 
-################## compute the kernels by examining the previous population of particles
-def getKernel(kern,population):
-    #print "\t\t***getKernel"
-
+# Compute the kernels WITHIN a model by examining the previous population of particles
+# populations, weights refers to particles and weights from previous population for one model
+def getKernel(kernel_type, kernel, population, weights):
     pop_size = population.shape[0]
-    #print population
-    #print "\t\tpop_size, params", pop_size, len(population[0])
-    
+    npar = population.shape[1]
+  
     if pop_size == 1:
-        return kern
+        return kernel
 
-    for param in range(len(population[0])):
-        #print "\t\tdist:", population[:,param]
-        #print "\t\told kernel", kern[param]
-        if kern[param][0]==1: 
-            # Uniform kernels
+    if kernel_type == 1:
+        # component-wise uniform kernels
+        for param in range(npar):
             minimum=min(population[:,param])
             maximum=max(population[:,param])
             scale=(maximum-minimum)
-            kern[param][1]=-scale/2.0
-            kern[param][2]=scale/2.0
-        if kern[param][0]==2:
-            # Gaussian kernels
-            var = var( population[:,param] ) # should be weighted
-            kern[param][2]=2*sqrt(var)
+            kernel[param][1]=-scale/2.0
+            kernel[param][2]=scale/2.0
 
-        #print "\t\tnew kernel", kern[param]
-    return kern
+    elif kernel_type == 2:
+        # component-wise normal kernels
+        for param in range(npar):
+            kernel[param][1] = 0
 
+            # optimal kernel
+            #s2 = numpy.var( population[:,param] ) # should be weighted
+            s2w = statistics.wtvar(population[:,param], weights, method = "R")
+            #print "vars:", s2, s2w
+
+            kernel[param][2]=2*s2w
+                    
+    print "\t\tnew kernel"
+    for p in range(npar):
+        print "\t\t", kernel[p]
+
+    return kernel
+
+# Here params refers to one particle
+# The function changes params in place and returns the probability (which may be zero)
 def perturbParticle(params, priors, kernel):
     np = len( priors )
     prior_prob = 1
 
     for n in range(0,np):
-         
-        if kernel[n][0]==1:
-            params[n] = params[n] + rnd.uniform(low=kernel[n][1],high=kernel[n][2])
+        if not(priors[n][0]==0):
+    
+            if kernel[n][0]==1:
+                params[n] = params[n] + rnd.uniform(low=kernel[n][1],high=kernel[n][2])
 
-        if kernel[n][0]==2: 
-            params[n] = params[n] + rnd.normal(kernel[n][1],kernel[n][2])
+            if kernel[n][0]==2: 
+                params[n] = params[n] + rnd.normal(kernel[n][1], numpy.sqrt(kernel[n][2]) )
 
-        x = 1.0
-        if priors[n][0]==2: 
-            x=statistics.getPdfUniform(priors[n][1],priors[n][2],params[n])
+            x = 1.0
+            if priors[n][0]==1: 
+                x=statistics.getPdfGauss(priors[n][1], numpy.sqrt(priors[n][2]), params[n])
 
-        if priors[n][0]==3: 
-            x=statistics.getPdfLognormal(priors[n][1],priors[n][2],params[n])
-            
-        prior_prob = prior_prob*x
+            if priors[n][0]==2: 
+                x=statistics.getPdfUniform(priors[n][1],priors[n][2],params[n])
+
+            if priors[n][0]==3: 
+                x=statistics.getPdfLognormal(priors[n][1],priors[n][2],params[n])
+
+            prior_prob = prior_prob*x
 
     return prior_prob
 
-def getPdfParameterKernel(params, params0, priors, kernel):
+# Here params and params0 refer to one particle each.
+# Auxilliary is a vector size of nparameters
+def getPdfParameterKernel(params, params0, priors, kernel, auxilliary):
     prob = 1
+
+    # loop over component-wise
     for n in range(0,len(priors)):
-        kern = 0
+        kern = 0.0
 
-        if not(priors[n][0]==0):
-                       
+        if not(priors[n][0]==0):     
             if kernel[n][0]==1: 
-                scale = (kernel[n][2]-kernel[n][1])/2.0
-                scale1 = max( (params0[n] - scale), priors[n][1])
-                scale2 = min( (params0[n] + scale), priors[n][2])
+                scale1 = params0[n] + kernel[n][1] # - range/2
+                scale2 = params0[n] + kernel[n][2] # + range/2
                 kern = statistics.getPdfUniform(scale1,scale2, params[n])
-
+               
             if kernel[n][0]==2:
                 mean = params0[n]
-                scale = kernel[n][2]
-                CDF2 = norm.cdf(priors[n][2],mean,scale)
-                CDF1 = norm.cdf(priors[n][1],mean,scale)
-                CDF = (CDF2-CDF1)**(-1)
+                scale = numpy.sqrt(kernel[n][2])
+                #CDF2 = norm.cdf(priors[n][2],mean,scale)
+                #CDF1 = norm.cdf(priors[n][1],mean,scale)
+                #CDF = (CDF2-CDF1)**(-1)
                 kern = statistics.getPdfGauss(mean,scale,params[n])
-                kern = kern*CDF
+                kern = kern/auxilliary[n]
 
         else: kern=1.0
         prob=prob*kern
 
+    #print prob
     return prob
 
+# Here models and parameters refer to the whole population
+def getAuxilliaryInfo(kernel_type, models, parameters, model_objs, kernel ):
+
+    nparticles = len(parameters)
+    ret = []
+      
+    if kernel_type == 2:
+        # component-wise Normal kernels
+        for k in range(nparticles):
+            this_prior = model_objs[models[k]].prior
+            this_kernel = kernel[models[k]]
+
+            nparam = len(this_prior)
+            ret.append( [1.0 for n in range(nparam)] )
+
+            for n in range(nparam):
+                if not(this_prior[n][0]==0):
+                    mean = parameters[k][n]
+                    scale = numpy.sqrt(this_kernel[n][2])
+                    ret[k][n] = norm.cdf(this_prior[n][2],mean,scale) - norm.cdf(this_prior[n][1],mean,scale)
+    else:
+        ret = [0 for i in  range(nparticles)]
+    
+    return ret
