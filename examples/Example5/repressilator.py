1713f26233773feac3c5e8129812e757285da23e
finalstate <finalstate@a9791ac1-d34e-45f5-b297-f83e2f7b00e5>
2011-07-21 14:31:24 +0000

    Added some numerical stability

--- examples/Example5/repressilator.py
+++ examples/Example5/repressilator.py
@@ -8,15 +8,22 @@ def modelfunction((m1,p1,m2,p2,m3,p3,),t,parameter=(1,2,5,1000,)):
 	n=parameter[1]
 	beta=parameter[2]
 	alpha=parameter[3]
-	
-	d_m1=-m1+alpha/(1+p3**n)+alpha0
+
+	if p3 > 0 : d_m1=-m1+alpha/(1 + pow(p3,n))+alpha0
+	else : d_m1=-m1+alpha+alpha0
+
 	d_p1=-beta*(p1-m1)
-	d_m2=-m2+alpha/(1+p1**n)+alpha0
+
+	if p1 > 0 : d_m2=-m2+alpha/(1 + pow(p1,n))+alpha0
+	else : d_m2=-m2+alpha+alpha0
+
 	d_p2=-beta*(p2-m2)
-	d_m3=-m3+alpha/(1+p2**n)+alpha0
+
+	if p2 > 0: d_m3=-m3+alpha/(1 + pow(p2,n))+alpha0
+	else : d_m3=-m3+alpha+alpha0
+
 	d_p3=-beta*(p3-m3)
-
-
+	
 	return(d_m1,d_p1,d_m2,d_p2,d_m3,d_p3, )
 
 
