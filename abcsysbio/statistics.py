874b7da3e1a75f7b7ee48a1ca9ea034baad1dddc
James Scott-Brown <james@jamesscottbrown.com>
2016-08-25 13:37:50 +0100

    Tidy up statistics.py - ranme variable,s add docstrings etc.

--- abcsysbio/statistics.py
+++ abcsysbio/statistics.py
@@ -2,14 +2,25 @@
 
 from numpy import *
 from numpy import random as rnd
-from numpy import linalg as LA
+from numpy import linalg as la
 import scipy
 import math
 import scipy.stats.mvn
 
 
-################## multinomial sampling
 def w_choice(item, weight):
+    """
+    Sample from the categorical distribution with probabilities given by weight.
+
+    Parameters
+    ----------
+    item : not used
+    weight : list of probability for each category
+
+    Returns
+    -------
+
+    """
     n = rnd.random_sample()
     for i in range(0, len(weight)):
         if n < weight[i]:
@@ -18,93 +29,218 @@ def w_choice(item, weight):
     return i
 
 
-################## compute the pdf of uniform distribution
-def getPdfUniform(scale1, scale2, parameter):
-    if (parameter > scale2) or (parameter < scale1):
+def getPdfUniform(min_val, max_val, x):
+    """
+    Evaluate the P(x) for x ~ U(min_val, max_val)
+
+    Parameters
+    ----------
+    x : value at which to evaluate the p.d.f
+    max_val : max value of uniform distribution
+    min_val : min value of uniform distribution
+    """
+    if (x > max_val) or (x < min_val):
         return 0.0
     else:
-        return 1 / (scale2 - scale1)
+        return 1 / (max_val - min_val)
 
 
-################## compute the pdf of gauss distribution
-def getPdfGauss(mean, scale, parameter):
-    x = exp(-0.5 * (parameter - mean) * (parameter - mean) / (scale * scale))
+def getPdfGauss(m, scale, x):
+    """
+    Evaluate P(x) for Gaussian distribution, x ~ N(m, scale^2)
+
+    Parameters
+    ----------
+    x : value at which to evaluate the p.d.f
+    scale : standard deviation of the Gaussian
+    m : mean of the Gaussian
+    """
+    x = exp(-0.5 * (x - m) * (x - m) / (scale * scale))
     x = x / (scale * sqrt(2 * pi))
     return x
 
 
-################ compute the pdf of lognormal distribution
-def getPdfLognormal(mean, sigm, parameter):
-    x = exp(-0.5 * (log(parameter) - mean) * (log(parameter) - mean) / (sigm * sigm))
-    x = x / (parameter * sigm * sqrt(2 * pi))
+def getPdfLognormal(m, sigma, x):
+    """
+    Evaluate P(x) for lognormal distribution, x ~ ln N(m, sigma^2)
+
+    N.B. If X is log-normal, then Y = ln(X) is normally distrbuted.
+
+    Parameters
+    ----------
+    x : value at which to evaluate the p.d.f
+    sigma : standard deviation of the associated normal
+    m : mean of the associated normal
+    """
+    x = exp(-0.5 * (log(x) - m) * (log(x) - m) / (sigma * sigma))
+    x = x / (x * sigma * sqrt(2 * pi))
     return x
 
 
-############ compute the pdf of a multinormal distribution
-def getPdfMultinormal(x, cov, y):
+# compute the pdf of a multinormal distribution
+def getPdfMultinormal(x, covariances, m):
+    """
+    Evaluate P(x) for multivariate normal distribution, x ~ N(means, covariances)
+
+    Parameters
+    ----------
+    x : value at which to evaluate the p.d.f
+    covariances : covariance matrix
+    m : mean vector
+
+    Returns
+    -------
+
+    """
     a = 0
     k = len(x)
-    inv = LA.inv(cov)
+    inv = la.inv(covariances)
     for i in range(k):
         for j in range(k):
-            a += inv[i, j] * (x[i] - y[i]) * (x[j] - y[j])
-    det = LA.det(cov)
+            a += inv[i, j] * (x[i] - m[i]) * (x[j] - m[j])
+    det = la.det(covariances)
     return exp(-1.0 * a / 2.0) / (sqrt((2 * pi) ** k * det))
 
 
-################ compute weighted variance
-################ http://adorio-research.org/wordpress/?p=259
-def wtvar(X, W, method="R"):
-    sumW = sum(W)
+def wtvar(x, weights, method="R"):
+    """
+    Compute the weighted variances for measurements x.
+
+    This function is based on http://adorio-research.org/wordpress/?p=259
+
+    Parameters
+    ----------
+    x : measurements
+    weights : weigths for each measurement
+    method : 'R' (Default) or 'nist'
+
+    Returns
+    -------
+    the weighted variance of the measurements
+    """
+    sum_w = sum(weights)
     if method == "nist":
-        xbarwt = sum([w * x for w, x in zip(W, X)]) / sumW  # fixed.2009.03.07, divisor added.
-        Np = sum([1 if (w != 0) else 0 for w in W])
-        D = sumW * (Np - 1.0) / Np
-        return sum([w * (x - xbarwt) ** 2 for w, x in zip(W, X)]) / D
-    else:  # default is R
-        sumW2 = sum([w ** 2 for w in W])
-        xbarwt = sum([(w * x) for (w, x) in zip(W, X)]) / sumW
-        return sum([(w * (x - xbarwt) ** 2) for (w, x) in zip(W, X)]) * sumW / (sumW ** 2 - sumW2)
+        x_bar_wt = sum([weights * x for weights, x in zip(weights, x)]) / sum_w  # fixed.2009.03.07, divisor added.
+        np = sum([1 if (weights != 0) else 0 for weights in weights])
+        d = sum_w * (np - 1.0) / np
+        return sum([weights * (x - x_bar_wt) ** 2 for weights, x in zip(weights, x)]) / d
+    else:
+        sum_w2 = sum([weights ** 2 for weights in weights])
+        x_bar_wt = sum([(weights * x) for (weights, x) in zip(weights, x)]) / sum_w
+        return sum([(weights * (x - x_bar_wt) ** 2) for (weights, x) in zip(weights, x)]) * sum_w / (
+            sum_w ** 2 - sum_w2)
 
 
-######### sample according to a multivariate normal
-def mvnd_gen(mean, cov):
-    a = list(rnd.normal(0, 1, len(mean)))
-    lambdas, vect = LA.eig(cov)
+def mvnd_gen(m, c):
+    """
+    Draw a sample from a multivariate normal distribution.
+
+    Parameters
+    ----------
+    m :  mean vector
+    c : covariance
+
+    Returns
+    -------
+    a sample from the distribution
+    """
+    a = list(rnd.normal(0, 1, len(m)))
+    lambdas, vect = la.eig(c)
     print lambdas
     tmp = mat(vect) * mat(diag(sqrt(lambdas))) * transpose(mat(a))
     res = list()
-    for i in range(len(mean)):
-        res.append(mean[i] + tmp[i, 0])
+    for i in range(len(m)):
+        res.append(m[i] + tmp[i, 0])
     return res
 
 
-################# compute the truncation factor for a multivariate normal kernel
-def mvstdnormcdf(lower, upper, corrcoef, **kwds):
+def mvstdnormcdf(lower, upper, corr_coef, **kwds):
+    """
+    standardized multivariate normal cumulative distribution function
+
+    This is a wrapper for scipy.stats.kde.mvn.mvndst which calculates
+    a rectangular integral over a standardized multivariate normal
+    distribution.
+
+    This function assumes standardized scale, that is the variance in each dimension
+    is one, but correlation can be arbitrary, covariance = correlation matrix
+
+    From statsmodels.sandbox.distributions.extras
+
+    Parameters
+    ----------
+    lower, upper : array_like, 1d
+       lower and upper integration limits with length equal to the number
+       of dimensions of the multivariate normal distribution. It can contain
+       -np.inf or np.inf for open integration intervals
+    corrcoef : float or array_like
+       specifies correlation matrix in one of three ways, see notes
+    optional keyword parameters to influence integration
+        * maxpts : int, maximum number of function values allowed. This
+             parameter can be used to limit the time. A sensible
+             strategy is to start with `maxpts` = 1000*N, and then
+             increase `maxpts` if ERROR is too large.
+        * abseps : float absolute error tolerance.
+        * releps : float relative error tolerance.
+
+    Returns
+    -------
+    cdfvalue : float
+        value of the integral
+
+
+    Notes
+    -----
+    The correlation matrix corrcoef can be given in 3 different ways
+    If the multivariate normal is two-dimensional than only the
+    correlation coefficient needs to be provided.
+    For general dimension the correlation matrix can be provided either
+    as a one-dimensional array of the upper triangular correlation
+    coefficients stacked by rows, or as full square correlation matrix
+
+    See Also
+    --------
+    mvnormcdf : cdf of multivariate normal distribution without
+        standardization
+
+    Examples
+    --------
+
+    >>> print mvstdnormcdf([-np.inf, -np.inf], [0.0, np.inf], 0.5)
+    0.5
+    >>> corr = [[1.0, 0, 0.5], [0, 1, 0], [0.5, 0,1]]
+    >>> print mvstdnormcdf([-np.inf, -np.inf, -100.0], [0.0, 0.0, 0.0], corr, abseps=1e-6)
+    0.166666399198
+    >>> print mvstdnormcdf([-np.inf, -np.inf, -100.0],[0.0, 0.0, 0.0], corr, abseps=1e-8)
+    something wrong completion with ERROR > EPS and MAXPTS function values used;
+                        increase MAXPTS to decrease ERROR; 1.048330348e-006
+    0.166666546218
+    >>> print mvstdnormcdf([-np.inf, -np.inf, -100.0], [0.0, 0.0, 0.0], corr, maxpts=100000, abseps=1e-8)
+    0.166666588293
+    """
+
     n = len(lower)
-    # don't know if converting to array is necessary,
-    # but it makes ndim check possible
+
     lower = array(lower)
     upper = array(upper)
-    corrcoef = array(corrcoef)
+    corr_coef = array(corr_coef)
 
     correl = zeros(n * (n - 1) / 2.0)  # dtype necessary?
 
     if (lower.ndim != 1) or (upper.ndim != 1):
         raise ValueError('can handle only 1D bounds')
+
     if len(upper) != n:
         raise ValueError('bounds have different lengths')
-    if n == 2 and corrcoef.size == 1:
-        correl = corrcoef
-        # print 'case scalar rho', n
-    elif corrcoef.ndim == 1 and len(corrcoef) == n * (n - 1) / 2.0:
-        # print 'case flat corr', corrcoeff.shape
-        correl = corrcoef
-    elif corrcoef.shape == (n, n):
-        # print 'case square corr',  correl.shape
+
+    if n == 2 and corr_coef.size == 1:
+        correl = corr_coef
+    elif corr_coef.ndim == 1 and len(corr_coef) == n * (n - 1) / 2.0:
+        correl = corr_coef
+    elif corr_coef.shape == (n, n):
         for ii in range(n):
             for jj in range(ii):
-                correl[jj + ((ii - 2) * (ii - 1)) / 2] = corrcoef[ii, jj]
+                correl[jj + ((ii - 2) * (ii - 1)) / 2] = corr_coef[ii, jj]
     else:
         raise ValueError('corrcoef has incorrect dimension')
 
@@ -123,83 +259,178 @@ def mvstdnormcdf(lower, upper, corrcoef, **kwds):
     error, cdfvalue, inform = scipy.stats.mvn.mvndst(lower, upper, infin, correl, **kwds)
     if inform:
         print 'something wrong', inform, error, cdfvalue
-        # print 'lower=', lower
-        # print 'upper=', upper
-        # print 'corrcoef=', corrcoef
     return cdfvalue
 
 
-def mvnormcdf(lower, upper, mu, cov, **kwds):
-    """find integral of multivariate normal for rectangular region"""
+def mvnormcdf(lower, upper, mu, c, **kwds):
+    """
+    multivariate normal cumulative distribution function
+
+    This is a wrapper for scipy.stats.kde.mvn.mvndst which calculates
+    a rectangular integral over a multivariate normal distribution.
+
+    From statsmodels.sandbox.distributions.extras
+
+    Parameters
+    ----------
+    lower, upper : array_like, 1d
+       lower and upper integration limits with length equal to the number
+       of dimensions of the multivariate normal distribution. It can contain
+       -np.inf or np.inf for open integration intervals
+    mu : array_lik, 1d
+       list or array of means
+    c : array_like, 2d
+       specifies covariance matrix
+    optional keyword parameters to influence integration
+        * maxpts : int, maximum number of function values allowed. This
+             parameter can be used to limit the time. A sensible
+             strategy is to start with `maxpts` = 1000*N, and then
+             increase `maxpts` if ERROR is too large.
+        * abseps : float absolute error tolerance.
+        * releps : float relative error tolerance.
+
+    Returns
+    -------
+    cdfvalue : float
+        value of the integral
+
+
+    Notes
+    -----
+    This function normalizes the location and scale of the multivariate
+    normal distribution and then uses `mvstdnormcdf` to call the integration.
+
+    See Also
+    --------
+    mvstdnormcdf : location and scale standardized multivariate normal cdf
+    """
 
     lower = array(lower)
     upper = array(upper)
-    cov = array(cov)
-    stdev = sqrt(diag(cov))
+    c = array(c)
+    stdev = sqrt(diag(c))
     lower = (lower - mu) / stdev
     upper = (upper - mu) / stdev
     divrow = atleast_2d(stdev)
-    corr = cov / divrow / divrow.T
-    # v/sqrt(atleast_2d(diag(covv)))/sqrt(atleast_2d(diag(covv))).T
+    corr = c / divrow / divrow.T
 
     return mvstdnormcdf(lower, upper, corr, **kwds)
 
 
-####### Compute the k nearest neighbors of a point inside a set S of points using the euclidian distance
-def kNearestNeighEuc(ind, S, k):
-    n = len(S[0])
-    SS = array(S)
-    aa = zeros((len(S), n))
-    for param in range(len(S)):
-        aa[param, :] = S[param][ind] * ones((1, n))
-    kmin = list()
-    dist = sum((aa - SS) ** 2, 2)
-    M = max(dist)
+def kNearestNeighEuc(ind, s, k):
+    """
+    Compute the k nearest neighbors of a point inside a set S of points using the Euclidian distance.
+
+    Parameters
+    ----------
+    ind : the index of the point within s whose nearest-neighbours are wanted
+    s : positions of points, s[param, point]
+    k : the number of nearest-neighbours to identify
+
+    Returns
+    -------
+
+    """
+    n = len(s[0])
+    ss = array(s)
+    aa = zeros((len(s), n))
+
+    for param in range(len(s)):
+        aa[param, :] = s[param][ind] * ones((1, n))
+
+    dist = sum((aa - ss) ** 2, 2)
+    m = max(dist)
+
+    k_min = list()
     for i in range(min(k, n)):
         im = argmin(dist)
-        kmin.append(im)
-        dist[im] = M
-    return kmin
+        k_min.append(im)
+        dist[im] = m
+    return k_min
 
 
-########### Compute the weighted covariance
-def compute_cov(population, w):
-    dimvar = len(population)
-    nbsamp = len(population[0])
-    cov = zeros([dimvar, dimvar], float)
+#
+def compute_cov(x, weights):
+    """
+        Compute the weighted covariance matrix for a set of measurements, by first calculating the weighted mean.
+
+
+    Parameters
+    ----------
+    x : measurements
+    weights : weights
+
+    Returns
+    -------
+
+    """
+    num_dimensions = len(x)
+    num_samples = len(x[0])
+    c = zeros([num_dimensions, num_dimensions], float)
+
+    # Calculate weighted mean of the samples
     m = list()
-    for k in range(dimvar):
+    for d in range(num_dimensions):
         m.append(0)
-        for i in range(nbsamp):
-            m[k] += w[i] * population[k][i]
-        m[k] = m[k] / sum(w)
-    for i in range(nbsamp):
-        for j in range(dimvar):
-            for k in range(j):
-                cov[j, k] += w[i] * (population[j][i] - m[j]) * (population[k][i] - m[k])
-    cov = cov + transpose(cov)
-    for i in range(nbsamp):
-        for j in range(dimvar):
-            cov[j, j] += w[i] * (population[j][i] - m[j]) * (population[j][i] - m[j])
-    for j in range(dimvar):
-        for k in range(dimvar):
-            cov[j, k] = cov[j, k] / sum(w)
-    return cov
+        for sample in range(num_samples):
+            m[d] += weights[sample] * x[d][sample]
+        m[d] = m[d] / sum(weights)
+
+    # Fill in upper half of c
+    for sample in range(num_samples):
+        for d1 in range(num_dimensions):
+            for d2 in range(d1):
+                c[d1, d2] += weights[sample] * (x[d1][sample] - m[d1]) * (x[d2][sample] - m[d2])
+
+    # Fill in lowe half by symmetry
+    c = c + transpose(c)
+
+    # Fill in diagonal
+    for sample in range(num_samples):
+        for d1 in range(num_dimensions):
+            c[d1, d1] += weights[sample] * (x[d1][sample] - m[d1]) * (x[d1][sample] - m[d1])
+
+    # Divide every element by the total weight
+    for d1 in range(num_dimensions):
+        for d2 in range(num_dimensions):
+            c[d1, d2] = c[d1, d2] / sum(weights)
+
+    return c
 
 
-#### compute the optimal covariance matrix
-def compute_optcovmat(population, w, p):
-    dimvar = len(population)
-    nbsamp = len(population[0])
-    C = zeros([dimvar, dimvar], float)
-    for n in range(nbsamp):
-        for i in range(dimvar):
-            for j in range(i):
-                C[i, j] += w[n] * (population[i][n] - p[i]) * (population[j][n] - p[j])
-    C = C + transpose(C)
-    for i in range(dimvar):
-        C[i, i] += w[n] * (population[i][n] - p[i]) * (population[i][n] - p[i])
-    for j in range(dimvar):
-        for k in range(dimvar):
-            C[j, k] = C[j, k] / sum(w)
-    return C
+def compute_optcovmat(x, weights, m):
+    """
+    Compute the weighted covariance matrix for a set of measurements, given means.
+
+    Parameters
+    ----------
+    x : measurements
+    weights : weights
+    m : mean
+
+    Returns
+    -------
+
+    """
+    num_dimensions = len(x)
+    num_samples = len(x[0])
+    c = zeros([num_dimensions, num_dimensions], float)
+
+    # Fill in upper-half of c
+    for sample in range(num_samples):
+        for d1 in range(num_dimensions):
+            for d2 in range(d1):
+                c[d1, d2] += weights[sample] * (x[d1][sample] - m[d1]) * (x[d2][sample] - m[d2])
+
+    # Fill in lower-half by symmetry
+    c = c + transpose(c)
+
+    # Fill in diagonal
+    for d1 in range(num_dimensions):
+        c[d1, d1] += weights[sample] * (x[d1][sample] - m[d1]) * (x[d1][sample] - m[d1])
+
+    # Divide every element by the total weight
+    for d2 in range(num_dimensions):
+        for k in range(num_dimensions):
+            c[d2, k] = c[d2, k] / sum(weights)
+    return c
