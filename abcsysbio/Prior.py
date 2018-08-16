3fb8abc621c60d00365397980acb2d367ac51444
James Scott-Brown <james@jamesscottbrown.com>
2018-08-16 18:48:25 +0100

    add field to tuple

--- abcsysbio/Prior.py
+++ abcsysbio/Prior.py
@@ -1,4 +1,4 @@
 from collections import namedtuple
 
-Prior = namedtuple('prior', ['type', 'value', 'mean', 'variance', 'lower_bound', 'upper_bound', 'mu', 'sigma'])
+Prior = namedtuple('prior', ['type', 'value', 'mean', 'variance', 'lower_bound', 'upper_bound', 'mu', 'sigma', 'p'])
 Prior.__new__.__defaults__ = (None,) * len(Prior._fields)
